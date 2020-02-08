ba25a70a92df2749294d1799fcc0260e146c1521
James <admin@James.local>
2020-02-09 00:34:42 +0300

    feat(error handling): handling application errors
    - catch errors from within the application
    
    - handling errors from external connections
    
    Delivers: #3245445

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 
 const app = express();
+const morgan = require("morgan");
 
+const tasksTodo = require("./api/routes/Tasks");
+
+app.use(morgan("dev"));
+
+app.use("/tasks", tasksTodo);
+
+// Handling errors within the application
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: "working"
+  const error = new Error("Invalid request");
+  error.status = 404;
+  next(error);
+});
+
+// Handling any error in the application from outside, i.e from db
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
   });
 });
 
