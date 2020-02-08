fd5df943b31c454cebf4040a2d81291f23cf96cd
James <admin@James.local>
2020-02-09 01:02:40 +0300

    feat(error handling): handling cors errors
    - added headers to handle CORS errors
    
    Finishes: #32445

--- api/routes/Tasks.js
+++ api/routes/Tasks.js
@@ -7,9 +7,20 @@ router.get("/", (req, res, next) => {
     message: "Many Tasks to do today"
   });
 });
+
 router.post("/", (req, res, next) => {
+  const task = {
+    item: req.body.item
+  };
+  res.status(201).json({
+    message: "Create a Task to do today",
+    TodoItems: task
+  });
+});
+
+router.delete("/", (req, res, next) => {
   res.status(200).json({
-    message: "Create a Task to do today"
+    message: "Delete"
   });
 });
 
--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const express = require("express");
 
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const tasksTodo = require("./api/routes/Tasks");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// headers to prevent CORS
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if ((req, method === "OPTIONS")) {
+    res.header("Access-Control-Allow-Methods", "PUT, PATCH, POST, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/tasks", tasksTodo);
 
